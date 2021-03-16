0c3bd10709fee10e67aa5a0c073c93a833a0983b
Franco Dominic Valdecanas <valdecan@sonoma.edu>
2021-03-16 14:22:32 -0700

    Ch24

---
+++ app/src/main/java/com/example/photogallery/api/FlickrApi.kt
@@ -0,0 +1,14 @@
+package com.example.photogallery.api
+
+import com.example.photogallery.FlickrResponse
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface FlickrApi {
+    @GET("services/rest/?method=flickr.interestingness.getList" +
+            "&api_key=b8f8f046810f6c43528f25d402b2e3f1" +
+            "&format=json" +
+            "&nojsoncallback=1" +
+            "&extras=url_s")
+    fun fetchPhotos(): Call<FlickrResponse>
+}
