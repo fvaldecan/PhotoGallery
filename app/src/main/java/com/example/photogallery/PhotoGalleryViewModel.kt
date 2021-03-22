bd5bf8d449dc88ebe20bf39e7cd5de2351840fbd
Franco Dominic Valdecanas <valdecan@sonoma.edu>
2021-03-21 22:09:00 -0700

    Ch26

--- app/src/main/java/com/example/photogallery/PhotoGalleryViewModel.kt
+++ app/src/main/java/com/example/photogallery/PhotoGalleryViewModel.kt
@@ -1,11 +1,28 @@
 package com.example.photogallery
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.lifecycle.*
 
-class PhotoGalleryViewModel : ViewModel(){
+private val flickrFetchr = FlickrFetchr()
+private val mutableSearchTerm = MutableLiveData<String>()
+
+class PhotoGalleryViewModel(private val app: Application) : AndroidViewModel(app) {
     val galleryItemLiveData: LiveData<List<GalleryItem>>
+    val searchTerm : String get() = mutableSearchTerm.value ?: ""
     init {
-        galleryItemLiveData = FlickrFetchr().fetchPhotos()
+        mutableSearchTerm.value = QueryPreferences.getStoredQuery(app)
+        galleryItemLiveData = Transformations.switchMap(mutableSearchTerm) { searchTerm ->
+            if (searchTerm.isBlank()) {
+                flickrFetchr.fetchPhotos()
+            } else {
+                flickrFetchr.searchPhotos(searchTerm)
+            }
+
+        }
+    }
+
+    fun fetchPhtos(query: String = "") {
+        QueryPreferences.setStoredQuery(app, query)
+        mutableSearchTerm.value = query
     }
 }
--- app/src/main/java/com/example/photogallery/api/FlickrApi.kt
+++ app/src/main/java/com/example/photogallery/api/FlickrApi.kt
@@ -4,17 +4,15 @@ import com.example.photogallery.FlickrResponse
 import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 import retrofit2.http.Url
 
 interface FlickrApi {
-    @GET("services/rest/?method=flickr.interestingness.getList" +
-            "&api_key=b8f8f046810f6c43528f25d402b2e3f1" +
-            "&format=json" +
-            "&nojsoncallback=1" +
-            "&extras=url_s")
-
+    @GET("services/rest?method=flickr.interestingness.getList")
     fun fetchPhotos(): Call<FlickrResponse>
     @GET
     fun fetchUrlBytes(@Url url: String): Call<ResponseBody>
+    @GET("services/rest?method=flickr.photos.search")
+    fun searchPhotos(@Query("text") query: String): Call<FlickrResponse>
 
 }
