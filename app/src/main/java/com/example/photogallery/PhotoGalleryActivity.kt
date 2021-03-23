61753552e6cf8447c92817cb6720087fdd7a615a
Franco Dominic Valdecanas <valdecan@sonoma.edu>
2021-03-22 21:43:57 -0700

    Ch27

--- app/src/main/java/com/example/photogallery/PhotoGalleryActivity.kt
+++ app/src/main/java/com/example/photogallery/PhotoGalleryActivity.kt
@@ -1,5 +1,7 @@
 package com.example.photogallery
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -13,6 +15,11 @@ class PhotoGalleryActivity : AppCompatActivity() {
                     .beginTransaction()
                     .add(R.id.fragmentContainer, PhotoGalleryFragment.newInstance())
                     .commit()
+        }
+    }
+    companion object {
+        fun newIntent(context: Context): Intent {
+            return Intent(context, PhotoGalleryActivity::class.java)
         }
     }
 }
--- app/src/main/java/com/example/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/example/photogallery/PhotoGalleryFragment.kt
@@ -17,9 +17,11 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.work.*
+import java.util.concurrent.TimeUnit
 
 private const val TAG = "PhotoGalleryFragment"
-
+private const val POLL_WORK = "POLL_WORK"
 class PhotoGalleryFragment : Fragment() {
     private lateinit var photoGalleryViewModel: PhotoGalleryViewModel
     private lateinit var photoRecyclerView: RecyclerView
@@ -37,6 +39,7 @@ class PhotoGalleryFragment : Fragment() {
                 photoHolder.bindDrawable(drawable)
             }
         lifecycle.addObserver(thumbnailDownloader.fragmentLifecycleObserver)
+
     }
 
     override fun onCreateView(
@@ -96,6 +99,14 @@ class PhotoGalleryFragment : Fragment() {
                 searchView.setQuery(photoGalleryViewModel.searchTerm, false)
             }
         }
+        val toggleItem = menu.findItem(R.id.menu_item_toggle_polling)
+        val isPolling = QueryPreferences.isPolling(requireContext())
+        val toggleItemTitle = if (isPolling) {
+            R.string.stop_polling
+        } else {
+            R.string.start_polling
+        }
+        toggleItem.setTitle(toggleItemTitle)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -104,7 +115,29 @@ class PhotoGalleryFragment : Fragment() {
                 photoGalleryViewModel.fetchPhtos("")
                 true
             }
+            R.id.menu_item_toggle_polling -> {
+                val isPolling = QueryPreferences.isPolling(requireContext())
+                if (isPolling) {
+                    WorkManager.getInstance().cancelUniqueWork(POLL_WORK)
+                    QueryPreferences.setPolling(requireContext(), false)
+                } else {
+                    val constraints = Constraints.Builder()
+                        .setRequiredNetworkType(NetworkType.UNMETERED)
+                        .build()
+                    val periodicRequest = PeriodicWorkRequest
+                        .Builder(PollWorker::class.java, 15, TimeUnit.MINUTES)
+                        .setConstraints(constraints)
+                        .build()
+                    WorkManager.getInstance().enqueueUniquePeriodicWork(POLL_WORK,
+                        ExistingPeriodicWorkPolicy.KEEP,
+                        periodicRequest)
+                    QueryPreferences.setPolling(requireContext(), true)
+                }
+                activity?.invalidateOptionsMenu()
+                return true
+            }
             else -> super.onOptionsItemSelected(item)
+
         }
     }
     private class PhotoHolder(private val itemImageView: ImageView) :
